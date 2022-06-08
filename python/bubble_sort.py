c9678d8eaff6a7d9b593b9e5d37ec3c0ecc87f68
Hunter McReynolds <mcreynoldshd@gmail.com>
2022-06-08 13:54:02 -0500

    initial py solution

--- python/bubble_sort.py
+++ python/bubble_sort.py
@@ -1,7 +1,24 @@
-sequence = [4, 3, 5, 0, 1]
-swaps = 0
 
-# Your Code Here
+def bubble_sort(sorting_list):
+    count=0
+    swaps= 0
+    while(True):
+        loop_swap = 0
+        for i in range(len(sorting_list)-1):
+            count +=1
+            print(f"{count} iterations")
+            previous = sorting_list[i]
+            current = sorting_list[i+1]
+            if(previous>current):
+                sorting_list[i+1] = previous
+                sorting_list[i] = current
+                swaps+=1
+                loop_swap +=1
+        if loop_swap == 0:
+            break
+    print(f"Final result: {sorting_list}")
+    print(f"Swaps: {swaps}")
+    
 
-print(f"Final result: {result}")
-print(f"Swaps: {swaps}")
+bubble_sort([4, 3, 5, 0, 1])
+
